b6691ac7be8f46c9a524cef4e20e08988bbaac79
adrianliaw <adrianliaw2000@gmail.com>
2017-02-26 17:09:33 +0800

    Fix typo and inconsistent tab/spaces

--- config/template.js
+++ config/template.js
@@ -1,8 +1,11 @@
 module.exports = {
-	mongoDB: {
-		url: 'mongodb://localhost:27017/ingestor',
-	},
-	influxDB: {
-		url: 'http://localhost:8086'
-	}
+  mongoDB: {
+    url: 'mongodb://localhost:27017/ingestor',
+  },
+  influxDB: {
+    url: 'http://localhost:8086'
+  },
+  deployer: {
+    url: 'http://localhost:7777'
+  }
 };
